ce6af510e2d2557de31c99587e64bb2dde57e074
ainulfi3 <ainulfitiri.jasni@pb.edu.bn>
2017-10-07 00:42:36 +0800

    add files

---
+++ app/src/main/java/com/example/cqaai/tutorpal/activity/ApplyTutor.java
@@ -0,0 +1,15 @@
+package com.example.cqaai.tutorpal.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.cqaai.tutorpal.R;
+
+public class ApplyTutor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apply_tutor);
+    }
+}
